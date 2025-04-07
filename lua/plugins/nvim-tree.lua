64189c43c75fb61ef86a7a18f4b1c54b8e9fb2d7
Harry Ganz <hganz@leagueapps.com>
2025-04-07 11:07:39 -0400

    Ok, actually show gitignored files in file-explorer

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,9 +1,9 @@
 return {
     "nvim-tree/nvim-tree.lua",
-    init = function()
-        local api = require("nvim-tree.api")
-        api.tree.toggle_gitignore_filter()
-    end,
-    config = true,
+    opts = {
+        git = {
+            ignore = false,
+        },
+    },
 }
 
