6063ab7eeaf6e9eadbf4c7007bcc6fcf8efbcae2
Harry Ganz <hganz@harryganz.com>
2025-04-06 22:18:40 -0400

    Adds new plugins
    * nvim window for nav
    * trouble for viewing diagnostics/usages
    * autopairs for auto bracket completion

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,18 +1,17 @@
--- Navigate between windows in terminal and normal mode using Alt-{direction}
-vim.keymap.set("t", "<A-h>", "<C-\\><C-N><C-w>h", { noremap = true, desc = "Navigate to left window" })
-vim.keymap.set("t", "<A-j>", "<C-\\><C-N><C-w>j", { noremap = true, desc = "Navigate to bottom window" })
-vim.keymap.set("t", "<A-k>", "<C-\\><C-N><C-w>k", { noremap = true, desc = "Navigate to top window" })
-vim.keymap.set("t", "<A-l>", "<C-\\><C-N><C-w>l", { noremap = true, desc = "Navigate to right window" })
-vim.keymap.set("n", "<A-h>", "<C-w>h", { noremap = true, desc = "Navigate to left window" })
-vim.keymap.set("n", "<A-j>", "<C-w>j", { noremap = true, desc = "Navigate to left window" })
-vim.keymap.set("n", "<A-k>", "<C-w>k", { noremap = true, desc = "Navigate to left window" })
-vim.keymap.set("n", "<A-l>", "<C-w>l", { noremap = true, desc = "Navigate to left window" })
+-- Map LSP actions
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename" })
+vim.keymap.set("n", '<leader>a', vim.lsp.buf.code_action, { desc = "View Code Actions"})
+vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, { desc = "Display information about current symbol"})
+
+-- Plugin based window Navigation
+local window = require("nvim-window")
+vim.keymap.set("n", "<leader>w", window.pick, { desc = "Navigate betwewn windows" })
 
 -- Toggle nvim-tree shortcut
 local nt = require("nvim-tree.api")
 vim.keymap.set("n", "<leader>t", nt.tree.toggle, {desc = "Toggle Navigation"})
 
--- Debug operatrions
+-- Debug operations
 local dap = require("dap")
 vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug continue" })
 vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug step over" })
@@ -32,4 +31,55 @@ vim.keymap.set("n", "<leader>fg", ts.live_grep, { desc = "Search within files" }
 vim.keymap.set("n", "<leader>fb", ts.buffers, { desc = "Search within buffers" })
 vim.keymap.set("n", "<leader>fh", ts.help_tags, { desc = "Search for help tags" })
 
+-- View Diagnostics/Symbols/Usages/QuickFixes
+local trouble = require("trouble")
 
+vim.keymap.set(
+    "n", 
+    "<leader>x", 
+    function() 
+        trouble.toggle({
+            mode = "diagnostics",
+            focus = false,
+            filter = { buf = 0 },
+        })
+    end, 
+    { desc = "Toggle diagnostics view"}
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>u",
+    function()
+        trouble.toggle({
+            mode = "lsp_references",
+            focus = true,
+        })
+    end,
+    { desc = "Toggle lsp references" }
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>df",
+    function()
+        trouble.toggle({
+            mode = "lsp_definitions",
+            focus = false,
+        })
+    end,
+    { desc = "Toggle lsp definitions" }
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>qf",
+    function()
+        trouble.toggle({
+            mode = "quickfix",
+            focus = true,
+            filter = { buf = 0 },
+        })
+    end,
+    { desc = "Toggle quick fixes" }
+)
