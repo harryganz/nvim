61c4083c3cc6669c264a29c257308b523c14638b
Harry Ganz <hganz@leagueapps.com>
2025-10-13 21:58:16 -0400

    Configures jdtls

--- lsp/jdtls.lua
+++ lsp/jdtls.lua
@@ -1,7 +1,11 @@
 return {
-    cmd = { "jdtls", "-data", vim.fn.stdpath("cache") .. "/jdtls/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t") },
+    cmd = {
+        "jdtls",
+        "-data", vim.fn.stdpath("cache") .. "/jdtls/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t"),
+        "-configuration", vim.fn.stdpath("cache") .. "/jdtls/config",
+    },
     filetypes = { "java" },
-    root_markers = { "pom.xml", "build.gradle.kts", "build.gradle.groovy", ".git" },
+    root_markers = { "pom.xml", "build.gradle.kts", "build.gradle", "build.gradle.groovy", "settings.gradle", ".git" },
     settings = {
         java = {
             import = {
@@ -14,7 +18,31 @@ return {
             },
             saveActions = {
                 organizeImports = true,
-            }
+            },
+            completion = {
+                favoriteStaticMembers = {
+                    "org.junit.jupiter.api.Assertions.*",
+                    "org.junit.Assert.*",
+                    "org.mockito.Mockito.*",
+                    "org.mockito.ArgumentMatchers.*",
+                    "java.util.Objects.requireNonNull",
+                    "java.util.Objects.requireNonNullElse",
+                },
+                filteredTypes = {
+                    "com.sun.*",
+                    "io.micrometer.shaded.*",
+                    "java.awt.*",
+                },
+            },
+            configuration = {
+                updateBuildConfiguration = "interactive",
+            },
+            referencesCodeLens = {
+                enabled = true,
+            },
+            implementationsCodeLens = {
+                enabled = true,
+            },
         },
     },
 }
