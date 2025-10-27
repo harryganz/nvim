aa3e7bdec48a27dccbdf79392f8aaca03ee47eb0
Harry Ganz <hganz@harryganz.com>
2025-10-26 20:05:39 -0400

    Integrates cspell lsp

---
+++ lsp/cspell-lsp.lua
@@ -0,0 +1,5 @@
+return {
+    cmd = { 'cspell-lsp', '--stdio' },
+    filetypes = { "go", "rust", "js", "ts", "html", "css", "json", "yaml", "markdown", "gitcommit" },
+    root_markers = { '.git' },
+}
